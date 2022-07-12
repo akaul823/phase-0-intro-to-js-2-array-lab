d5f1144bb4a59ba34bf428875dcdf4dae763bab5
Avi Kaul <avikarkaul@gmail.com>
2022-07-12 16:42:34 -0400

    commit tree 552c61afc862e38c25d14a51e2072ea464da766e

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    return cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    return cats.shift(name);
+}
+
+function appendCat(name){
+    const newCat = [...cats, "Broom"];
+    return newCat;
+}
+
+function prependCat(name){
+    const newCat = ["Arnold", ...cats];
+    return newCat;
+}
+
+function removeLastCat(){
+    const newCat = [...cats.slice(0,2)];
+    return newCat;
+}
+
+function removeFirstCat(){
+    const newCat = [...cats.slice(1,3)];
+    console.log(newCat);
+    return newCat;
+}
+
+
